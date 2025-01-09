85fdc6cf8e8bd91c4f1a420dc40575e3a816fa61
사용자명 <>
2025-01-09 09:24:10 +0900

    샘플데이터 작업 완료

--- chart_gen/src/FileUpload.js
+++ chart_gen/src/FileUpload.js
@@ -27,7 +27,7 @@ function FileUpload({ onBack, onDataParsed }) {
             <div className="top_title_box n2">
                 <button className='custom-btn custom-back btn btn-secondary' onClick={onBack}>뒤로가기</button>
                 <strong className='title'>파일 첨부</strong>
-                <div></div>
+                <a href={`${process.env.PUBLIC_URL}/file/sample_data.zip`} download="sample_data.zip" className="btn btn-success btn_download">샘플 다운로드</a>
             </div>
             <label for="file" className='item_box_v1'>
                 <div className="btn-upload">파일 업로드하기</div>
