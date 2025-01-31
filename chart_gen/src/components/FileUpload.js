9966654aa96c08cdc27f080f202987c059130daa
사용자명 <>
2025-01-31 13:31:00 +0900

    샘플 다운로드 버튼 메인화면으로 이동

--- chart_gen/src/components/FileUpload.js
+++ chart_gen/src/components/FileUpload.js
@@ -21,7 +21,7 @@ function FileUpload({ onBack, onDataParsed }) {
             <div className="top_title_box n2">
                 <button className='custom-btn custom-back btn btn-secondary' onClick={onBack}>뒤로가기</button>
                 <strong className='title'>파일 첨부</strong>
-                <a href={`${process.env.PUBLIC_URL}/file/sample_data.zip`} download="sample_data.zip" className="btn btn-success btn_download">샘플 다운로드</a>
+                <div className="dummy"></div>
             </div>
             <label for="file" className='item_box_v1'>
                 <div className="btn-upload text-center">
